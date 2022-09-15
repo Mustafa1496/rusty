b13bc3ed1ce573669c059bcdfcc1166c8258ee55
Mustafa_Cakir <cakirmustafa018@gmail.com>
2022-09-15 13:13:52 +0300

    added int

--- src/constant.rs
+++ src/constant.rs
@@ -2,7 +2,7 @@
 
 fn main(){
     const PI: f64 = 3.1415;
-    let radius = 2.0;   
+    let radius = 2;   
     let radius = radius as f64;
     println!("area of 2 square meter circle is {} square meter.",PI*radius*radius);
 }
