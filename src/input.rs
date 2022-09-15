41d6f611a6d581b0e9259b6d45162d6f56c19d89
Mustafa_Cakir <cakirmustafa018@gmail.com>
2022-09-15 12:56:09 +0300

    deleted

--- src/input.rs
+++ src/input.rs
@@ -3,13 +3,4 @@ use std::io;
 
 fn main(){
 
-    println!("enter a name");
-
-    let mut name = String::new();
-
-    io::stdin()
-    .read_line(&mut name)
-    .expect("didnt read a line");
-
-    println!("hello {}",name);
 }
