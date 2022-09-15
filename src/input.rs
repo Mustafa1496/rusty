a0cebb4d46c38073ac9f950d7d211410f0f6c842
Mustafa_Cakir <cakirmustafa018@gmail.com>
2022-09-15 12:32:11 +0300

    take inputs

---
+++ src/input.rs
@@ -0,0 +1,15 @@
+
+use std::io;
+
+fn main(){
+
+    println!("enter a name");
+
+    let mut name = String::new();
+
+    io::stdin()
+    .read_line(&mut name)
+    .expect("didnt read a line");
+
+    println!("hello {}",name);
+}
