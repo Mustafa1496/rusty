4e5f26dbd7335e29fa534ba9cf246861d7363546
Mustafa_Cakir <cakirmustafa018@gmail.com>
2022-09-15 13:13:17 +0300

    added message

--- src/constant.rs
+++ src/constant.rs
@@ -4,5 +4,5 @@ fn main(){
     const PI: f64 = 3.1415;
     let radius = 2.0;   
     let radius = radius as f64;
-    println!("{}",PI*radius*radius);
+    println!("area of 2 square meter circle is {} square meter.",PI*radius*radius);
 }
